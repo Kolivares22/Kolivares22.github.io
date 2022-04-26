5a79a2cf330b9cce5930256a4ae29cb3c7b13e90
Olivares Kayly <833886@stma.k12.mn.us>
2022-04-26 12:28:10 -0500

    iphone

---
+++ stma.js
@@ -0,0 +1,46 @@
+function Activities(){
+	window.open("https://stmaknights.com/")
+}
+document.getElementById("activities").addEventListener ("click", Activities)
+
+function Calendar(){
+	window.open("https://www.stma.k12.mn.us/Page/2")
+}
+document.getElementById("calendar").addEventListener ("click", Calendar)
+function Yearly(){
+	window.open("https://www.stma.k12.mn.us/domain/4555")
+}
+document.getElementById("yearly").addEventListener ("click", Yearly)
+function Student(){
+	window.open("https://pvue7.region1.k12.mn.us/rt0885/")
+}
+document.getElementById("student").addEventListener ("click", Student)
+function Menu (){
+	window.open("https://stma.nutrislice.com/menus-eula")
+}
+document.getElementById("menus").addEventListener ("click", Menu)
+function Jobs(){
+	window.open("https://www.stma.k12.mn.us/domain/2433")
+}
+document.getElementById("jobs").addEventListener ("click", Jobs)
+function Sky(){
+	window.open("https://pac.stma.k12.mn.us/scripts/wsisa.dll/WService=wsFin/seplog01.w")
+}
+document.getElementById("sky").addEventListener ("click", Sky)
+function Email(){
+	window.open("https://mail.google.com/mail/u/0/#inbox")
+}
+document.getElementById("email").addEventListener ("click", Email)
+
+
+
+
+
+
+
+
+
+
+
+
+
