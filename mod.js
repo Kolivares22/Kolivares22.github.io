6a752ffde2d9fea1eaf26a596f5877192d0e60e3
Olivares Kayly <833886@stma.k12.mn.us>
2022-05-12 12:26:56 -0500

    modified stma wsite

---
+++ mod.js
@@ -0,0 +1,22 @@
+function District(){
+	window.open("https://www.stma.k12.mn.us/domain/77")
+}
+document.getElementById("dis").addEventListener("click", District)
+
+function Department(){
+	window.open("https://www.stma.k12.mn.us/domain/4601")
+}
+document.getElementById("dep").addEventListener("click",Department)
+
+function Teach(){
+	window.open("https://www.stma.k12.mn.us/domain/4553")
+}
+document.getElementById("tea").addEventListener("click",Teach)
+
+function Enroll(){
+	window.open("https://www.stma.k12.mn.us/domain/3445")
+}
+document.getElementById("en").addEventListener("click", Enroll)
+
+
+
