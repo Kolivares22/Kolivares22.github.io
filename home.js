db8577283c3d1c5e1b1c38e0e1b4d87648956c67
Olivares Kayly <833886@stma.k12.mn.us>
2022-05-31 12:45:24 -0500

    culture

--- home.js
+++ home.js
@@ -2,3 +2,22 @@ function Arco(){
 	window.open("https://en.wikipedia.org/wiki/Arco_de_Santa_Catalina")
 }
 document.getElementById("arco").addEventListener ("click", Arco)
+
+var count= 0;
+function One(){
+	count = count + 7.68;
+	document.getElementById("total").innerHTML=count;
+}
+document.getElementById("One").addEventListener("click", One);
+
+function Ten(){
+	count = count + 76.79;
+	document.getElementById("total").innerHTML=count;
+}
+document.getElementById("Ten").addEventListener("click", Ten);
+
+
+
+
+
+
