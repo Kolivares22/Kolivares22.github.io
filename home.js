ab2d7fbad21a467c93de933319ccffd4b46ab955
Kolivares22 <102545758+Kolivares22@users.noreply.github.com>
2022-06-02 17:29:10 -0500

    trying to fix quiz

--- home.js
+++ home.js
@@ -1,96 +1,30 @@
-function Arco(){
-	window.open("https://en.wikipedia.org/wiki/Arco_de_Santa_Catalina")
+
+function check(){
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+	var question4 = document.quiz.question4.value;
+	var question5 = document.quiz.question5.value;
+	var question6 = document.quiz.question6.value;
+	var correct = 0;
+
+if (question1 =="Spain") {
+	correct++;
 }
-document.getElementById("arco").addEventListener ("click", Arco)
-
-function checkSpain(){
-	
-
-if (document.getElementById("password").value == "Spain"){
-	document.getElementById("access").innerHTML = "CORRECT";
-	document.getElementById("access").style.color = "green";
+if (question2 =="Hotel Casa Santo Domingo") {
+	correct++;
 }
-	else{
-		document.getElementById("access").innerHTML = "The password " +
-		document.getElementById("password").value + " was the wrong.";
-		document.getElementById("access").style.color = "red";
-	}
+if (question3 =="Old") {
+	correct++;
 }
-
-document.getElementById("submit").addEventListener("click", checkSpain);
-
-function checkHotel(){
-
-if (document.getElementById("passwor").value == "Hotel Casa San Domingo"){
-	document.getElementById("acces").innerHTML = "CORRECT";
-	document.getElementById("acces").style.color = "green";
+if (question4 =="El Sombreron") {
+	correct++;
 }
-	else{
-		document.getElementById("acces").innerHTML = "The password " +
-		document.getElementById("passwor").value + " was the wrong.";
-		document.getElementById("acces").style.color = "red";
-	}
+if (question5 =="Coffee") {
+	correct++;
 }
-document.getElementById("submi").addEventListener("click", checkHotel);
-
-
-function checkAntigua(){
-
-if (document.getElementById("passwo").value == "Old"){
-	document.getElementById("acce").innerHTML = "CORRECT";
-	document.getElementById("acce").style.color = "green";
-}
-	else{
-		document.getElementById("acce").innerHTML = "The password " +
-		document.getElementById("passwo").value + " was the wrong.";
-		document.getElementById("acce").style.color = "red";
-	}
-}
-document.getElementById("subm").addEventListener("click", checkAntigua);
-
-
-function checkFolk(){
-
-if (document.getElementById("passw").value == "El Sombreron"){
-	document.getElementById("acc").innerHTML = "CORRECT";
-	document.getElementById("acc").style.color = "green";
-}
-	else{
-		document.getElementById("acc").innerHTML = "The password " +
-		document.getElementById("passw").value + " was the wrong.";
-		document.getElementById("acc").style.color = "red";
-	}
+if (question6 =="Ayarza") {
+	correct++;
 }
 
-document.getElementById("sub").addEventListener("click", checkFolk);
-
-function checkRegion(){
-
-if (document.getElementById("pass").value == "Coffee"){
-	document.getElementById("ac").innerHTML = "CORRECT";
-	document.getElementById("ac").style.color = "green";
 }
-	else{
-		document.getElementById("ac").innerHTML = "The password " +
-		document.getElementById("pass").value + " was the wrong.";
-		document.getElementById("ac").style.color = "red";
-	}
-}
-
-document.getElementById("su").addEventListener("click", checkRegion);
-
-function checkFamily(){
-
-if (document.getElementById("pas").value == "Guatemala"){
-	document.getElementById("a").innerHTML = "CORRECT";
-	document.getElementById("a").style.color = "green";
-}
-	else{
-		document.getElementById("a").innerHTML = "The password " +
-		document.getElementById("pas").value + " was the wrong.";
-		document.getElementById("a").style.color = "red";
-	}
-}
-
-document.getElementById("s").addEventListener("click", checkFamily);
-
